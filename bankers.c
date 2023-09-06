01daa901dbee88fd17d37ead3906dbd591746162
Murali Krishna <mmallela9@gmail.com>
2023-09-06 07:22:29 +0530

    first commit

--- bankers.c
+++ bankers.c
@@ -1,66 +1,49 @@
 #include <stdio.h>
-int main()
-{
-    
-    int n, m, i, j, k;
-    n = 5;                        
-    m = 3;                         
-    int alloc[5][3] = {{0, 1, 0},  
-                       {2, 0, 0},  
-                       {3, 0, 2},
-                       {2, 1, 1},  
-                       {0, 0, 2}};
+#include <stdbool.h>
 
+int main() {
+    int n = 5, m = 3;
+    int alloc[5][3] = {{0, 1, 0}, 
+    {2, 0, 0},
+    {3, 0, 2},
+    {2, 1, 1}, 
+    {0, 0, 2}};
     int max[5][3] = {{7, 5, 3}, 
-                     {3, 2, 2}, 
-                     {9, 0, 2},  
-                     {2, 2, 2}, 
-                     {4, 3, 3}}; 
+    {3, 2, 2}, 
+    {9, 0, 2}, 
+    {2, 2, 2}, 
+    {4, 3, 3}};
+    int avail[3] = {3, 3, 2};
+    bool finish[5] = {false};
+    int ans[5];
+    int ind = 0;
 
-    int avail[3] = {3, 3, 2}; 
-
-    int f[n], ans[n], ind = 0;
-    for (k = 0; k < n; k++)
-    {
-        f[k] = 0;
-    }
-    int need[n][m];
-    for (i = 0; i < n; i++)
-    {
-        for (j = 0; j < m; j++)
-            need[i][j] = max[i][j] - alloc[i][j];
-    }
-    int y = 0;
-    for (k = 0; k < 5; k++)
-    {
-        for (i = 0; i < n; i++)
-        {
-            if (f[i] == 0)
-            {
-
-                int flag = 0;
-                for (j = 0; j < m; j++)
-                {
-                    if (need[i][j] > avail[j])
-                    {
-                        flag = 1;
+    for (int k = 0; k < n; k++) {
+        for (int i = 0; i < n; i++) {
+            if (!finish[i]) {
+                bool canAllocate = true;
+                for (int j = 0; j < m; j++) {
+                    if (max[i][j] - alloc[i][j] > avail[j]) {
+                        canAllocate = false;
                         break;
                     }
                 }
-
-                if (flag == 0)
-                {
+                if (canAllocate) {
+                    for (int j = 0; j < m; j++) {
+                        avail[j] += alloc[i][j];
+                    }
                     ans[ind++] = i;
-                    for (y = 0; y < m; y++)
-                        avail[y] += alloc[i][y];
-                    f[i] = 1;
+                    finish[i] = true;
                 }
             }
         }
     }
-    printf("Following is the SAFE Sequence\n");
-    for (i = 0; i < n - 1; i++)
-        printf(" P%d ->", ans[i]);
-    printf(" P%d", ans[n - 1]);
-    return (0);
+
+    printf("Safe Sequence: ");
+    for (int i = 0; i < n - 1; i++) {
+        printf("P%d -> ", ans[i]);
+    }
+    printf("P%d\n", ans[n - 1]);
+
+    return 0;
 }
